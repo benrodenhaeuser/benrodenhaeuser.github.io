b07e8cd4f0de6bcc69007cb198ca19dbce29bb7b
Ben Rodenhäuser <ben@karo.berlin>
2018-03-08 15:00:13 +0100

    set up scss

---
+++ _site/assets/jax/config.js
@@ -0,0 +1,14 @@
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
+  "HTML-CSS" : {
+    availableFonts : ["Asana-Math"],
+    preferredFont : "Asana-Math",
+    webFont : "Asana-Math",
+    imageFont : null,
+    scale: 90
+  },
+  styles: {
+    ".MathJax": {color: "#333"},
+  }
+});
+MathJax.Hub.Configured();
